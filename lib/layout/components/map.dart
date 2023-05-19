7437cb8f344a490141a26f6e32b1ab93d1b1d56f
Rechberger, Jakob <jakob.rechberger@student.fhws.de>
2023-05-19 17:42:47 +0200

    feat: mapbox integration (not stable)

--- lib/layout/components/map.dart
+++ lib/layout/components/map.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+
 
 class MapBox extends StatelessWidget {
-  const MapBox({Key? key}) :
-        super(key: key);
+  final LatLng latLng;
+  const MapBox({Key? key, required this.latLng}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Map Overview')),
-      body: const Center(
-        child: Text('integrate MapBox API'),
+      body: Stack(
+        children: [
+          MapboxMap(
+            initialCameraPosition: CameraPosition(
+            target: latLng,
+              zoom: 13,
+            ),
+            onMapCreated: (controller) {controller.addSymbol(const SymbolOptions(
+                geometry: LatLng(37.7749, -122.4194), // Example marker position
+                iconImage: 'marker-icon', // Example marker icon
+                ));},
+            accessToken: 'sk.eyJ1IjoiamFrb2JyZWNoYmVyZ2VyIiwiYSI6ImNsaHVtYjFtdjAxOTAzbnJ3OGFqczRtNTkifQ.IbS4Q7hu_0cldmzM4Sr3Jw',
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Increment',
@@ -19,4 +34,19 @@ class MapBox extends StatelessWidget {
 
     );
   }
+}
+void addMarker(MapboxMapController controller, LatLng latLng) async {
+  var byteData = await rootBundle.load("images/poi.png");
+  var markerImage = byteData.buffer.asUint8List();
+
+  controller.addImage('marker', markerImage);
+
+  await controller.addSymbol(
+    SymbolOptions(
+      iconSize: 0.3,
+      iconImage: "marker",
+      geometry: latLng,
+      iconAnchor: "bottom",
+    ),
+  );
 }
---
+++ lib/layout/map_integration.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'sk.eyJ1IjoiamFrb2JyZWNoYmVyZ2VyIiwiYSI6ImNsaHVtYjFtdjAxOTAzbnJ3OGFqczRtNTkifQ.IbS4Q7hu_0cldmzM4Sr3Jw';
+
+  static const String mapBoxStyleId = 'YOUR_STYLE_ID';
+
+  static final myLocation = LatLng(51.5090214, -0.1982948);
+}
